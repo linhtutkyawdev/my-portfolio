27f46e5741cdf0f65fbe330f32f0d1dbbbfadd63
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2023-10-20 05:49:06 +0630

    configured theme edited some

---
+++ src/constants/index.ts
@@ -0,0 +1,70 @@
+export const NAME = "Lin Htut Kyaw";
+export const LINKS = ["Home", "About", "Services", "Brsolr", "Blog", "Contact"];
+export const DESCRIPTION = "A Full-Stack Web Developer";
+export const SKILLS: { title: string; progress: number }[] = [
+  {
+    title: "Javascript (Vanilla)",
+    progress: 80,
+  },
+  {
+    title: "PHP (Vanilla)",
+    progress: 60,
+  },
+  {
+    title: "Nodejs",
+    progress: 95,
+  },
+  {
+    title: "ReactJS",
+    progress: 97,
+  },
+  {
+    title: "NextJS",
+    progress: 95,
+  },
+  {
+    title: "AstroJS",
+    progress: 90,
+  },
+  {
+    title: "QwikJS",
+    progress: 80,
+  },
+  {
+    title: "TailwindCSS",
+    progress: 90,
+  },
+  {
+    title: "Laravel",
+    progress: 70,
+  },
+  {
+    title: "Python",
+    progress: 60,
+  },
+  {
+    title: "HTML",
+    progress: 100,
+  },
+
+  {
+    title: "CSS",
+    progress: 98,
+  },
+  {
+    title: "SevelteJS",
+    progress: 75,
+  },
+  {
+    title: "Express",
+    progress: 75,
+  },
+  {
+    title: "ElysiaJS",
+    progress: 75,
+  },
+  {
+    title: "BunSH",
+    progress: 100,
+  },
+];
