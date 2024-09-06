aa2ffd786a2840ad76b161a4048034897c17af6b
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2024-09-06 19:58:19 +0630

    small upd

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,85 +1,97 @@
-export const NAME = "Lin Htut Kyaw";
-export const LINKS = ["Home", "About", "Services", "Works", "Blog", "Contact"];
-export const DESCRIPTION = "A Full-Stack Web Developer";
+export const NAME = 'Lin Htut Kyaw';
+export const LINKS = ['Home', 'About', 'Services', 'Works', 'Blog', 'Contact'];
+export const DESCRIPTION = 'A Creative Developer';
 export const SKILLS: { title: string; progress: number }[] = [
   {
-    title: "Javascript / Typescript (Vanilla)",
+    title: 'Golang',
+    progress: 88,
+  },
+  {
+    title: 'PHP (Vanilla)',
+    progress: 89,
+  },
+  {
+    title: 'Rust',
+    progress: 77,
+  },
+  {
+    title: 'C#',
+    progress: 74,
+  },
+  {
+    title: 'Java',
+    progress: 81,
+  },
+  {
+    title: 'Git',
     progress: 87,
   },
   {
-    title: "Rust",
-    progress: 75,
+    title: 'Docker',
+    progress: 92,
   },
   {
-    title: "C#",
-    progress: 70,
-  },
-  {
-    title: "Java",
-    progress: 80,
-  },
-  {
-    title: "PHP (Vanilla)",
-    progress: 75,
-  },
-  {
-    title: "Bash",
-    progress: 70,
-  },
-  {
-    title: "Linux",
-    progress: 80,
-  },
-  {
-    title: "Python",
-    progress: 70,
-  },
-  {
-    title: "Laravel",
-    progress: 75,
-  },
-  {
-    title: "Nodejs",
-    progress: 90,
-  },
-  {
-    title: "ExpressJS",
-    progress: 75,
-  },
-  {
-    title: "ReactJS",
-    progress: 95,
-  },
-  {
-    title: "NextJS",
-    progress: 95,
-  },
-  {
-    title: "AstroJS",
-    progress: 90,
-  },
-  {
-    title: "QwikJS",
-    progress: 80,
-  },
-  {
-    title: "SevelteJS",
-    progress: 75,
-  },
-  {
-    title: "ElysiaJS",
-    progress: 70,
-  },
-  {
-    title: "HTML",
+    title: 'Bash',
     progress: 99,
   },
   {
-    title: "CSS",
+    title: 'Linux',
+    progress: 81,
+  },
+  {
+    title: 'Python',
+    progress: 87,
+  },
+  {
+    title: 'Laravel',
+    progress: 84,
+  },
+  {
+    title: 'Javascript / Typescript (Vanilla)',
     progress: 99,
   },
   {
-    title: "TailwindCSS",
+    title: 'Nodejs',
+    progress: 99,
+  },
+  {
+    title: 'ReactJS',
+    progress: 97,
+  },
+  {
+    title: 'NextJS',
+    progress: 96,
+  },
+  {
+    title: 'AstroJS',
+    progress: 94,
+  },
+  {
+    title: 'QwikJS',
+    progress: 82,
+  },
+  {
+    title: 'SevelteJS',
+    progress: 78,
+  },
+  {
+    title: 'ExpressJS',
+    progress: 89,
+  },
+  {
+    title: 'ElysiaJS',
+    progress: 73,
+  },
+  {
+    title: 'HTML',
+    progress: 99,
+  },
+  {
+    title: 'CSS',
+    progress: 99,
+  },
+  {
+    title: 'TailwindCSS',
     progress: 99,
   },
 ];
