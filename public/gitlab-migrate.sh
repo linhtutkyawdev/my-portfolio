6044c75d8963be7a40f375710c3cbcba81254e61
Lin Htut Kyaw <linhtutkyaw.dev@gmail.com>
2025-01-17 18:41:01 +0630

    added gitlab-migrate cli serving

---
+++ public/gitlab-migrate.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+sudo curl -Lo /usr/bin/gitlab-migrate https://github.com/linhtutkyawdev/gitlab-migrate/releases/download/v1.0.3/gitlab-migrate_linux_amd64
+sudo chmod +x /usr/bin/gitlab-migrate
